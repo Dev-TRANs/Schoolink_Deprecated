c3b81f91e0c44ed6bbbcf08ee23f192f5f76b9d7
-akku- <151517253+akku1139@users.noreply.github.com>
2023-12-18 19:34:25 +0900

    '@/' でimprtできるように

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,21 @@ import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
 
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    }
+  }
 });
